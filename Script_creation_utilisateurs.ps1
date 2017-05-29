bd0d9bd722fb95d346af4c6667e751ba612e6212
nabil2199 <nabil2199@gmail.com>
2017-05-29 15:38:12 +0200

    Asssign License + unforce password change

--- Script_creation_utilisateurs.ps1
+++ Script_creation_utilisateurs.ps1
@@ -7,6 +7,9 @@ BSD 3-Clause License
 
 param([string]$userCsv = "C:\Sources\Liste_Utilisateurs_creation.csv")
 
+#Setting Account SKU Id for tenant
+$MSOLSKUid=(Get-MsolAccountSku).accountskuid
+
 #User CSV loading
 $usersList = $null
 $usersList = Import-Csv $userCsv
@@ -16,8 +19,11 @@ Write-Host ""
 
 foreach ($user in $usersList)
 {
-    Write-Host -NoNewline "creation de l'utilisateur: "; Write-Host -NoNewline -ForegroundColor Cyan $user.DisplayName; Write-Host -NoNewline " avec le mot de passe"; Write-Host -ForegroundColor Green $user.Password
+    Write-Host -NoNewline "creation de l'utilisateur: "; Write-Host -NoNewline -ForegroundColor Cyan $user.DisplayName; Write-Host -NoNewline " avec le mot de passe "; Write-Host -ForegroundColor Green $user.Password
+#Creating user
     New-MsolUser -DisplayName $user.DisplayName -FirstName $user.FirstName -LastName $user.LastName -UserPrincipalName $user.UserPrincipalName -UsageLocation "Fr" -Password $user.Password
+#Assigning license to user
+    Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -AddLicenses $MSOLSKUid
+#Setting user password without forced change
     Set-MsolUserPassword -UserPrincipalName $user.UserPrincipalName -ForceChangePassword $false -NewPassword $user.Password 
-
 }
